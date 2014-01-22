85b9bda9559b2520b1d5fed9cdc56f9d459242cd
Scott Bader <sb@scottbader.org>
2014-01-22 14:07:50 -0500

    Initial Commit

---
+++ Tackle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tackle
+//
+//  Created by Scott Bader on 1/22/14.
+//  Copyright (c) 2014 Melody Road. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TackAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TackAppDelegate class]));
+    }
+}
---
+++ TackleTests/TackleTests.m
@@ -0,0 +1,34 @@
+//
+//  TackleTests.m
+//  TackleTests
+//
+//  Created by Scott Bader on 1/22/14.
+//  Copyright (c) 2014 Melody Road. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TackleTests : XCTestCase
+
+@end
+
+@implementation TackleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
