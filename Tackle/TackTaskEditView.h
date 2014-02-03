b1765d71989801f0b75b83ffb702b83242656ecc
Scott Bader <sb@scottbader.org>
2014-02-03 18:54:40 -0500

    Adding date picker

--- Tackle/TackTaskEditView.h
+++ Tackle/TackTaskEditView.h
@@ -19,6 +19,8 @@
 @property (strong, nonatomic) UIButton *addOneDayButton;
 @property (strong, nonatomic) UIButton *submitButton;
 @property (strong, nonatomic) UIButton *dueDateButton;
+@property (strong, nonatomic) UIDatePicker *datePicker;
+@property (strong, nonatomic) UIView *bottomButtonView;
 @property (strong, nonatomic) NSDate *dueDate;
 
 - (void)resetContent;
--- Tackle/TackTaskEditView.m
+++ Tackle/TackTaskEditView.m
@@ -10,6 +10,12 @@
 
 #import "TackDateFormatter.h"
 
+@interface TackTaskEditView ()
+
+@property (nonatomic) BOOL isDatePickerShown;
+
+@end
+
 @implementation TackTaskEditView
 
 - (id)initWithFrame:(CGRect)frame
@@ -22,10 +28,9 @@
 
         [self setupTextField];
         [self setupDueDateButton];
-        [self setupAddTenMinutesButton];
-        [self setupAddOneHourButton];
-        [self setupAddOneDayButton];
-        [self setupSubmitButton];
+        [self setupBottomButtonView];
+        [self setupDatePicker];
+        [self updateDueDate];
 
         [self addObservers];
     }
@@ -63,7 +68,7 @@
     self.dueDateButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 51.0f, self.frame.size.width, 56.0f)];
     [self.dueDateButton setTitleColor:UIColorFromRGB(0x7091BC) forState:UIControlStateNormal];
     [self.dueDateButton.titleLabel setFont:[UIFont adonisRegularWithSize:23.0f]];
-    [self updateDueDate];
+    [self.dueDateButton addTarget:self action:@selector(displayDatePicker:) forControlEvents:UIControlEventTouchUpInside];
 
     UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 107.0f, self.frame.size.width, 1.0f)];
     [separatorView setBackgroundColor:UIColorFromRGB(0xCACACA)];
@@ -72,67 +77,89 @@
     [self addSubview:separatorView];
 }
 
+- (void)setupBottomButtonView
+{
+    self.bottomButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 108, self.frame.size.width, 59.0f)];
+
+    [self setupAddTenMinutesButton];
+    [self setupAddOneHourButton];
+    [self setupAddOneDayButton];
+    [self setupSubmitButton];
+
+    [self addSubview:self.bottomButtonView];
+}
+
+- (void)setupDatePicker
+{
+    self.isDatePickerShown = NO;
+
+    self.datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 108.0f, self.frame.size.width, 120.0f)];
+    [self.datePicker setHidden:YES];
+    [self.datePicker addTarget:self action:@selector(datePickerChanged:) forControlEvents:UIControlEventValueChanged];
+    [self addSubview:self.datePicker];
+}
+
 - (void)setupAddTenMinutesButton
 {
-    self.addTenMinutesButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 108.0f, 109.0f, 59.0f)];
+    self.addTenMinutesButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 109.0f, 59.0f)];
     [self.addTenMinutesButton setTitle:@"+ 10 Minutes" forState:UIControlStateNormal];
     [self.addTenMinutesButton setTitleColor:UIColorFromRGB(0x7091BC) forState:UIControlStateNormal];
     [self.addTenMinutesButton.titleLabel setFont:[UIFont adonisRegularWithSize:15.5f]];
 
     [self.addTenMinutesButton addTarget:self action:@selector(handleAddTenMinutes:) forControlEvents:UIControlEventTouchUpInside];
 
-    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(108.0f, 108.0f, 1.0f, 57.0f)];
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(108, 0, 1.0f, 57.0f)];
     [separatorView setBackgroundColor:UIColorFromRGB(0xCACACA)];
 
-    [self addSubview:self.addTenMinutesButton];
-    [self addSubview:separatorView];
+    [self.bottomButtonView addSubview:self.addTenMinutesButton];
+    [self.bottomButtonView addSubview:separatorView];
 }
 
 - (void)setupAddOneHourButton
 {
-    self.addOneHourButton = [[UIButton alloc] initWithFrame:CGRectMake(109.0f, 108.0f, 79.0f, 59.0f)];
+    self.addOneHourButton = [[UIButton alloc] initWithFrame:CGRectMake(109.0f, 0, 79.0f, 59.0f)];
     [self.addOneHourButton setTitle:@"+ 1 Hour" forState:UIControlStateNormal];
     [self.addOneHourButton setTitleColor:UIColorFromRGB(0x7091BC) forState:UIControlStateNormal];
     [self.addOneHourButton.titleLabel setFont:[UIFont adonisRegularWithSize:15.5f]];
 
     [self.addOneHourButton addTarget:self action:@selector(handleAddOneHour:) forControlEvents:UIControlEventTouchUpInside];
 
-    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(187.0f, 108.0f, 1.0f, 57.0f)];
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(187.0f, 0, 1.0f, 57.0f)];
     [separatorView setBackgroundColor:UIColorFromRGB(0xCACACA)];
 
-    [self addSubview:self.addOneHourButton];
-    [self addSubview:separatorView];
+    [self.bottomButtonView addSubview:self.addOneHourButton];
+    [self.bottomButtonView addSubview:separatorView];
 }
 
 - (void)setupAddOneDayButton
 {
-    self.addOneDayButton = [[UIButton alloc] initWithFrame:CGRectMake(188.0f, 108.0f, 72.0f, 59.0f)];
+    self.addOneDayButton = [[UIButton alloc] initWithFrame:CGRectMake(188.0f, 0, 72.0f, 59.0f)];
     [self.addOneDayButton setTitle:@"+ 1 Day" forState:UIControlStateNormal];
     [self.addOneDayButton setTitleColor:UIColorFromRGB(0x7091BC) forState:UIControlStateNormal];
     [self.addOneDayButton.titleLabel setFont:[UIFont adonisRegularWithSize:15.5f]];
 
     [self.addOneDayButton addTarget:self action:@selector(handleAddOneDay:) forControlEvents:UIControlEventTouchUpInside];
 
-    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(259.0f, 108.0f, 1.0f, 57.0f)];
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(259.0f, 0, 1.0f, 57.0f)];
     [separatorView setBackgroundColor:UIColorFromRGB(0xCACACA)];
 
-    [self addSubview:self.addOneDayButton];
-    [self addSubview:separatorView];
+    [self.bottomButtonView addSubview:self.addOneDayButton];
+    [self.bottomButtonView addSubview:separatorView];
 }
 
 - (void)setupSubmitButton
 {
-    self.submitButton = [[UIButton alloc] initWithFrame:CGRectMake(260.0f, 108.0f, 61.0f, 59.0f)];
+    self.submitButton = [[UIButton alloc] initWithFrame:CGRectMake(260.0f, 0, 61.0f, 59.0f)];
     [self.submitButton setTitle:@"Go" forState:UIControlStateNormal];
     [self.submitButton setTitleColor:UIColorFromRGB(0x2E67B3) forState:UIControlStateNormal];
     [self.submitButton.titleLabel setFont:[UIFont adonisRegularWithSize:29.5f]];
     [self.submitButton addTarget:self action:@selector(handleSubmit:) forControlEvents:UIControlEventTouchUpInside];
 
-    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height - 1, self.frame.size.width, 1.0f)];
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0, self.bottomButtonView.frame.size.height - 2, self.bottomButtonView.frame.size.width, 1.0f)];
     [separatorView setBackgroundColor:UIColorFromRGB(0xCACACA)];
 
-    [self addSubview:self.submitButton];
-    [self addSubview:separatorView];
+    [self.bottomButtonView addSubview:self.submitButton];
+    [self.bottomButtonView addSubview:separatorView];
 }
 
 - (void)handleSubmit:(id)sender
@@ -157,15 +184,70 @@
     [self setDueDate:[NSDate dateWithTimeInterval:86400 sinceDate:self.dueDate]];
 }
 
+- (void)displayDatePicker:(id)sender
+{
+    CGRect mainFrame = self.frame;
+    CGRect bottomButtonViewFrame = self.bottomButtonView.frame;
+
+    if (self.isDatePickerShown) {
+        mainFrame.size.height = mainFrame.size.height - 216;
+        bottomButtonViewFrame.origin.y = bottomButtonViewFrame.origin.y - 216;
+
+        [self.datePicker setHidden:YES];
+        self.isDatePickerShown = NO;
+
+        [UIView animateWithDuration:0.2 animations:^{
+            [self.bottomButtonView setFrame:bottomButtonViewFrame];
+            [self setFrame:mainFrame];
+        }];
+
+    }
+    else {
+        if (self.textField.isFirstResponder) {
+            [self.textField resignFirstResponder];
+        }
+
+        mainFrame.size.height = mainFrame.size.height + 216;
+        bottomButtonViewFrame.origin.y = bottomButtonViewFrame.origin.y + 216;
+
+        [UIView animateWithDuration:0.2 animations:^{
+            [self setFrame:mainFrame];
+            [self.bottomButtonView setFrame:bottomButtonViewFrame];
+        } completion:^(BOOL finished) {
+            [self.datePicker setHidden:NO];
+            self.isDatePickerShown = YES;
+        }];
+    }
+}
+
+- (void)datePickerChanged:(id)sender
+{
+    self.dueDate = self.datePicker.date;
+    [self updateDueDate];
+}
+
 - (void)updateDueDate
 {
     [self.dueDateButton setTitle:[[TackDateFormatter sharedInstance] stringFromDate:self.dueDate] forState:UIControlStateNormal];
+    [self.datePicker setDate:self.dueDate];
 }
 
 - (void)resetContent
 {
     [self setDueDate:[NSDate dateWithTimeIntervalSinceNow:600]];
     [self.textField setText:@""];
+    if (self.isDatePickerShown) {
+        CGRect mainFrame = self.frame;
+        CGRect bottomButtonViewFrame = self.bottomButtonView.frame;
+
+        mainFrame.size.height = mainFrame.size.height - 216;
+        bottomButtonViewFrame.origin.y = bottomButtonViewFrame.origin.y - 216;
+
+        [self.datePicker setHidden:YES];
+        self.isDatePickerShown = NO;
+        [self.bottomButtonView setFrame:bottomButtonViewFrame];
+        [self setFrame:mainFrame];
+    }
 }
 
 #pragma mark - NSKeyValueObserving
