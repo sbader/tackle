2f8742475d9a24c85f53f5d081edac923b04b19e
Scott Bader <sb@scottbader.org>
2014-02-02 21:59:29 -0500

    Cleaning up

---
+++ Tackle/UIView+TackAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIView+TackAdditions.h
+//  Tackle
+//
+//  Created by Scott Bader on 2/2/14.
+//  Copyright (c) 2014 Melody Road. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (TackAdditions)
+
+- (CGFloat)angleOfView;
+
+@end
