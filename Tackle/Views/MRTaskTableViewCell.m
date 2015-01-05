dd9d7f6e43e8b950fc2ed981eceb96b9195ac3f5
Scott Bader <sb@scottbader.org>
2015-01-04 20:21:25 -0500

    fixing main table view cell

--- Tackle/Views/MRTaskTableViewCell.m
+++ Tackle/Views/MRTaskTableViewCell.m
@@ -11,7 +11,7 @@
 @implementation MRTaskTableViewCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIdentifier];
+    self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
 
     if (self) {
         self.layoutMargins = UIEdgeInsetsZero;
@@ -37,28 +37,28 @@
 }
 
 - (void)setupConstraints {
-//    self.textLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.detailTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//
-//    [self.contentView addCompactConstraints:@[
-//                                              @"text.leading = view.leading + horizontalPadding",
-//                                              @"text.trailing = view.trailing - horizontalPadding",
-//                                              @"detail.leading = view.leading + horizontalPadding",
-//                                              @"detail.trailing = view.trailing - horizontalPadding",
-//                                              @"text.top = view.top + verticalPadding",
-//                                              @"detail.top = text.bottom + spacing",
-//                                              @"detail.bottom = view.bottom - verticalPadding",
-//                                              ]
-//                                    metrics:@{
-//                                              @"horizontalPadding": @(20),
-//                                              @"verticalPadding": @(14),
-//                                              @"spacing": @(2)
-//                                              }
-//                                      views:@{
-//                                              @"text": self.textLabel,
-//                                              @"detail": self.detailTextLabel,
-//                                              @"view": self.contentView
-//                                              }];
+    self.textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.detailTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.contentView addCompactConstraints:@[
+                                              @"text.leading = view.leading + horizontalPadding",
+                                              @"text.trailing = view.trailing - horizontalPadding",
+                                              @"detail.leading = view.leading + horizontalPadding",
+                                              @"detail.trailing = view.trailing - horizontalPadding",
+                                              @"text.top = view.top + verticalPadding",
+                                              @"detail.top = text.bottom + spacing",
+                                              @"detail.bottom = view.bottom - verticalPadding",
+                                              ]
+                                    metrics:@{
+                                              @"horizontalPadding": @(20),
+                                              @"verticalPadding": @(14),
+                                              @"spacing": @(2)
+                                              }
+                                      views:@{
+                                              @"text": self.textLabel,
+                                              @"detail": self.detailTextLabel,
+                                              @"view": self.contentView
+                                              }];
 }
 
 @end
